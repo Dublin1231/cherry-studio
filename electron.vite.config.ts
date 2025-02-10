ee900bfb28c7506a5369c593f5f0082bcfa087fa
Dublin1231 <3564710846@qq.com>
2025-02-10 23:49:26 +0800

    refactor: 更新npmrc和yarnrc配置，调整electron版本及依赖

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -7,10 +7,15 @@ const visualizerPlugin = (type: 'renderer' | 'main') => {
   return process.env[`VISUALIZER_${type.toUpperCase()}`] ? [visualizer({ open: true })] : []
 }
 
+process.env.ELECTRON_MIRROR = 'https://npmmirror.com/mirrors/electron/'
+process.env.ELECTRON_CUSTOM_DIR = '31.7.6'
+process.env.ELECTRON_SKIP_BINARY_DOWNLOAD = '0'
+
 export default defineConfig({
   main: {
     plugins: [
       externalizeDepsPlugin({
+        include: ['electron'],
         exclude: [
           '@llm-tools/embedjs',
           '@llm-tools/embedjs-openai',
@@ -29,27 +34,44 @@ export default defineConfig({
       alias: {
         '@main': resolve('src/main'),
         '@types': resolve('src/renderer/src/types'),
-        '@shared': resolve('packages/shared')
+        '@shared': resolve('packages/shared'),
+        electron: resolve('node_modules/electron')
       }
     },
     build: {
       rollupOptions: {
-        external: ['@libsql/client']
+        external: ['@libsql/client', 'electron']
       }
     }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [
+      externalizeDepsPlugin({
+        include: ['electron']
+      })
+    ],
+    resolve: {
+      alias: {
+        electron: resolve('node_modules/electron')
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: ['electron']
+      }
+    }
   },
   renderer: {
     plugins: [react(), ...visualizerPlugin('renderer')],
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
-        '@shared': resolve('packages/shared')
+        '@shared': resolve('packages/shared'),
+        electron: resolve('node_modules/electron')
       }
     },
     optimizeDeps: {
+      include: ['electron'],
       exclude: ['chunk-PZ64DZKH.js', 'chunk-JMKENWIY.js']
     }
   }
